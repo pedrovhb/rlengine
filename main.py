41addb72876d3b6740a6be8169b1fe5bdfaad4c1
Pedro Batista <pedrovhb@gmail.com>
2020-05-29 11:04:43 -0300

    Make tile dimensions more generic

--- main.py
+++ main.py
@@ -1,38 +1,60 @@
+from dataclasses import dataclass
+
 import pyglet
 import typing as T
 from pyglet.image import TextureRegion, gl
 from pyglet.window import key
 
-
 window = pyglet.window.Window()
+
+
+@dataclass
+class TileDimensions:
+    tile_width: int = 16
+    tile_height: int = 16
+    sheet_border_x: int = 0
+    sheet_border_y: int = 0
+    tile_margin_x: int = 1
+    tile_margin_y: int = 1
 
 
 class TiledImageMap:
 
-    def __init__(self, tiles_filepath: str, tile_width=16, tile_height=16):
+    def __init__(
+            self,
+            tiles_filepath: str,
+            tile_dimensions: TileDimensions = None
+    ):
+        self.tile_dimensions = tile_dimensions or TileDimensions()
         self.tiles = pyglet.resource.image(tiles_filepath)
-        self.tile_width, self.tile_height = tile_width, tile_height
         self.textures: T.Dict[T.Tuple[int, int]] = {}
 
         for x in range(self.horizontal_tiles):
             for y in range(self.vertical_tiles):
-                self.textures[x, y] = self.tiles.get_region(
-                    x * self.tile_width + x,
-                    y * self.tile_height + y,
-                    self.tile_width,
-                    self.tile_height,
+                tex_x = (
+                        x * self.tile_dimensions.tile_width
+                        + x * self.tile_dimensions.tile_margin_x
+                        + self.tile_dimensions.tile_margin_x
                 )
+                tex_y = (
+                        y * self.tile_dimensions.tile_height
+                        + y * self.tile_dimensions.tile_margin_y
+                        + self.tile_dimensions.tile_margin_y
+                )
+                w = self.tile_dimensions.tile_width
+                h = self.tile_dimensions.tile_height
+                self.textures[x, y] = self.tiles.get_region(tex_x, tex_y, w, h)
 
     def __getitem__(self, item: T.Tuple[int, int]) -> TextureRegion:
         return self.textures[item]
 
     @property
     def horizontal_tiles(self) -> int:
-        return self.tiles.width // self.tile_width
+        return self.tiles.width // self.tile_dimensions.tile_width
 
     @property
     def vertical_tiles(self) -> int:
-        return self.tiles.height // self.tile_height
+        return self.tiles.height // self.tile_dimensions.tile_height
 
 
 @window.event
